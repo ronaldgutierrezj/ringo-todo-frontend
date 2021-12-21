25dd0a6622013baef6b56a8be548a6e3331ffe2b
ronald <ronaldgutierrezj@gmail.com>
2021-12-21 18:20:08 -0500

    6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "milligram";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Route path="/" component= {App} />
+  </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
